53dacce2a7db738d4950da49b299056cbd9e964a
Alex Browne <stephenalexbrowne@gmail.com>
2015-05-15 22:26:32 -0400

    Add package doc comment

---
+++ doc.go
@@ -0,0 +1,10 @@
+// Package view is a small library for organizing view-related code written in pure go
+// which compiles to javascript via gopherjs (github.com/gopherjs/gopherjs). View includes
+// a View interface and some helper functions for operating on views (e.g. Append, Replace,
+// Remove, etc.).
+//
+// Version X.X.X (develop)
+//
+// For the full source code, a getting started guide, and more information visit
+// https://github.com/go-humble/view.
+package view
